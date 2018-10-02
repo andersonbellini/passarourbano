d6abe8d051465c3976b488e57b47632ecd14f0ef
anderson.bellini <anderson.bellini@anac.gov.br>
2018-10-02 13:02:35 -0300

    Utilizando Subscribe para recuperação

--- passarourbano/src/app/oferta/oferta.component.ts
+++ passarourbano/src/app/oferta/oferta.component.ts
@@ -11,9 +11,18 @@ export class OfertaComponent implements OnInit {
   constructor(private route: ActivatedRoute) {  }
 
   ngOnInit() {
-    this.route.snapshot.params['id'];
-
+    // *** Usando SnapShot
+    //this.route.snapshot.params['id'];
     //this.route.snapshot.params['subId'];  // Caso necessário uso de subId
+
+    // *** Usando subscribe
+    this.route.paramMap.subscribe((parametro: any) => {
+      //console.log(parametro.id);
+
+    });
+
+
   }
+
 
 }
