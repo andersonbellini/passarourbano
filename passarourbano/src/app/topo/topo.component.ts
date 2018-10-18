0f381f450e86dee24dda6219bac06cc0273f6871
anderson.bellini <anderson.bellini@anac.gov.br>
2018-10-18 16:23:17 -0300

    Adicionado debounceTime para fazer a requisição após X milessegundos

--- passarourbano/src/app/topo/topo.component.ts
+++ passarourbano/src/app/topo/topo.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
 
 import { switchMap } from 'rxjs/internal/operators/switchMap';
+import { debounceTime } from 'rxjs/operators';
 
 import { OfertasService } from '../ofertas.service';
 import { Oferta } from '../shared/oferta.model';
@@ -22,6 +23,7 @@ export class TopoComponent implements OnInit {
   ngOnInit() {
      //retorno Oferta[]
       this.ofertas = this.subjectPesquisa.pipe(
+        debounceTime(1000),
         switchMap((termo: string) => {
           console.log('Requisição http para API: ', termo);
           return this.ofertasService.pesquisaOfertas(termo);
